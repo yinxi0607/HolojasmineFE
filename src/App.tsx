b24783f44e001f2306f55f8d9ad0d4a023c11160
niuhongbin <niuhongbin111@163.com>
2023-08-18 15:22:29 +0800

    add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Layout} from "antd";
-import {Content, Header,Footer} from "antd/es/layout/layout";
-
+import {Layout } from "antd";
+import {Content, Header, Footer} from "antd/es/layout/layout";
+import {InstagramOutlined} from "@ant-design/icons"
+import "./App.css"
 
 const headerStyle: React.CSSProperties = {
-    textAlign: 'center',
-    // color: '#fff',
-    height: "5vh",
+    // textAlign: 'center',
+    // // color: '#fff',
+    height: "12vh",
     paddingInline: 50,
     // lineHeight: '64px',
     backgroundColor: 'white',
@@ -16,78 +17,75 @@ const headerStyle: React.CSSProperties = {
 const footerStyle: React.CSSProperties = {
     textAlign: 'center',
     backgroundColor: 'white',
-    height: "5vh",
-    fontSize: "2vw",
+    height: "15vh",
+    // fontSize: "2vw",
 };
 
 const contentStyle: React.CSSProperties = {
     textAlign: 'center',
-    // minHeight: "20vh",
-    // lineHeight: '120px',
     display: "flex",
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     color: 'black',
     backgroundColor: '#fff',
-    height: "88vh",
-
+    marginTop: "-8vh",
+    height: "76vh",
 };
 
+const containerStyle: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start', // 设置为左对齐
+    textAlignLast: "justify",
+    textAlign: "justify"
+};
+
+//
+// const instagramIconStyle: React.CSSProperties = {
+//     fontSize: '1.5vw',
+//     position: 'absolute',
+//     top: '0.5vh',
+//     right: '1vw',
+// };
+
 const HomePage: React.FC = () => {
-    // const [lineIndex, setLineIndex] = useState(0);
-    //
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         setLineIndex((prevIndex) => (prevIndex + 1) % 20);
-    //     }, 2000); // 每2秒切换一行
-    //
-    //     return () => clearInterval(interval);
-    // }, []);
-    //
-    // const lines = [
-    //     "Line 1",
-    //     "Line 2",
-    //     "Line 3",
-    //     // ... 其他行 ...
-    //     "Line 20",
-    // ];
-    // const handleReplay = () => {
-    //     setLineIndex(0); // 重新设置行索引为0，从第一行开始滚动
-    // };
-
-
     return (
-        // <div>
-        //     <button className="button" onClick={handleReplay}>
-        //         RESET
-        //     </button>
-        //
-        //
-        //     <div style={{ overflow: 'hidden', height: '1.5em' }}>
-        //         <div style={{ transform: `translateY(-${lineIndex * 1.5}em)` }}>
-        //             {lines.map((line, index) => (
-        //                 <div className="text" key={index}>{line}</div>
-        //             ))}
-        //         </div>
-        //     </div>
-        // </div>
-        // <div style={{ position: "absolute", justifyContent: "center", textAlign: "center", width: "100vw", height: "100vh", display: "flex", alignItems: "center", fontSize: "10vw" }}>
-        //     Hello World!
-        // </div>
-
         <Layout>
-            <Header style={headerStyle}>这里添加一个大logo</Header>
+            <Header style={headerStyle}>
+                <a href="https://www.instagram.com/your_instagram_account" target="_blank" rel="noopener noreferrer">
+                    <InstagramOutlined style={{position: 'absolute',top: '1.5vh',right: '2.5vw',}}/>
+                </a>
+            </Header>
             <Content style={contentStyle}>
-                <div style={{
-                    fontSize: "10vw"}}>
-                     Hello World!
-                </div>
-                <div style={{ marginTop: '1vh', fontSize: '2vw' }}>Welcome to my website</div>
-            </Content>
-            <Footer style={footerStyle}>这里需要有一些超链接!</Footer>
-        </Layout>
+                <div style={containerStyle}>
+                    <div style={{
+                        fontSize: "2vw", fontFamily: "SpaceMonoBold",width:"50vw"
+                    }}>
+                        FROM NO THINGS COMES A GREAT MULTITUDE
 
+                    </div>
+                    <div style={{
+                        marginTop: '2vh', fontSize: '2vw', fontFamily: "SourceHanMonoBold",width:"50vw"
+                        // letterSpacing: '5vh'
+                    }}>
+                        ⽆中⽣有 万物盈天
+                    </div>
+                </div>
+
+            </Content>
+            <Footer style={footerStyle}>
+                <div style={{marginTop:"-15vh", fontSize:"2.2vw",fontFamily:"SourceHanMonoRegular"}}>
+                    COME WITH US
+                </div>
+                <div style={{marginTop:"1vh", fontSize:"1vw",fontFamily:"SpaceMonoBold"}}>
+                    info@holojasmine.com
+                </div>
+                <div style={{marginTop:"10vh", fontSize:"1vw",marginBottom:"-10vh",fontFamily:"SourceHanMonoBold"}}>
+                    BEIJING / SHANGHAI / HONGKONG / TAIBEI / JAKARTA / TOKYO / BANGKOK  / SEOUL
+                </div>
+            </Footer>
+        </Layout>
     )
 };
 
